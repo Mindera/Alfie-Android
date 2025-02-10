d0a9fa8abf1aa6cf3cd848e665ef5b2238dfa910
marcofreitas <marco.freitas@mindera.com>
2025-02-10 17:08:11 +0000

    On progress WishlistUiFactoryTest.kt and WishlistViewModelTest.kt

--- feature/bag/src/test/java/au/com/alfie/ecomm/feature/bag/BagUiFactoryTest.kt
+++ feature/bag/src/test/java/au/com/alfie/ecomm/feature/bag/BagUiFactoryTest.kt
@@ -3,9 +3,10 @@ package au.com.alfie.ecomm.feature.bag
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.Test
 import kotlin.test.assertEquals
+
 
 @ExtendWith(MockKExtension::class)
 class BagUiFactoryTest {
@@ -14,7 +15,7 @@ class BagUiFactoryTest {
     private lateinit var uiFactory: BagUiFactory
 
     @Test
-    fun `map bag to ui xSmall`() = runTest {
+    fun `map wishlist to ui xSmall`() = runTest {
         val result = uiFactory(products)
 
         assertEquals(result, bagProductUi)
