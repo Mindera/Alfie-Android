35fe89475c33c62eda293ac7de971665dcb9d8e1
mashhood.qazi <mashhood.qazi@mindera.com>
2025-02-17 20:38:09 +0000

    chore: fixed tests

--- domain/src/test/java/au/com/alfie/ecomm/domain/bag/GetBagUseCaseTest.kt
+++ domain/src/test/java/au/com/alfie/ecomm/domain/bag/GetBagUseCaseTest.kt
@@ -11,6 +11,8 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -27,11 +29,10 @@ class GetBagUseCaseTest {
     @Test
     fun `get list of products in the bag`() = runTest {
         val mockBag = mockk<List<BagProduct>>()
-        coEvery { bagRepository.getBag() } returns RepositoryResult.Success(mockBag)
+        coEvery { bagRepository.getBag() } returns flowOf(RepositoryResult.Success(mockBag))
 
         val expected = UseCaseResult.Success(mockBag)
-
-        val result = subject()
+        val result = subject().first()
 
         assertEquals(expected, result)
     }
@@ -39,11 +40,10 @@ class GetBagUseCaseTest {
     @Test
     fun `get list of products in the bag, and returns an error`() = runTest {
         val errorResult = mockk<ErrorResult>()
-        coEvery { bagRepository.getBag() } returns RepositoryResult.Error(errorResult)
+        coEvery { bagRepository.getBag() } returns flowOf(RepositoryResult.Error(errorResult))
 
         val expected = UseCaseResult.Error(errorResult)
-
-        val result = subject()
+        val result = subject().first()
 
         assertEquals(expected, result)
     }
