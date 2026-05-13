41ca79f06743a7419b82aab2a8ed55a31b589965
Nhat Hoang <nhat.hoang@mindera.com>
2026-05-13 14:09:47 +0700

    Make detekt happy

--- feature/wishlist/src/test/java/au/com/alfie/ecomm/feature/bag/WishlistViewModelTest.kt
+++ feature/wishlist/src/test/java/au/com/alfie/ecomm/feature/bag/WishlistViewModelTest.kt
@@ -2,10 +2,10 @@ package au.com.alfie.ecomm.feature.bag
 
 import androidx.lifecycle.SavedStateHandle
 import app.cash.turbine.test
-import au.com.alfie.ecomm.core.ui.event.ClickEventOneArg
 import au.com.alfie.ecomm.core.navigation.Screen
 import au.com.alfie.ecomm.core.navigation.arguments.productDetailsNavArgs
 import au.com.alfie.ecomm.core.test.CoroutineExtension
+import au.com.alfie.ecomm.core.ui.event.ClickEventOneArg
 import au.com.alfie.ecomm.domain.UseCaseResult
 import au.com.alfie.ecomm.domain.usecase.wishlist.GetWishlistUseCase
 import au.com.alfie.ecomm.domain.usecase.wishlist.RemoveFromWishlistUseCase
@@ -43,98 +43,103 @@ internal class WishlistViewModelTest {
     private lateinit var uiEventEmitterDelegate: UIEventEmitterDelegate
 
     @Test
-    fun `WHEN getWishlistList returns a success THEN update the state with the correct wishlist items`() = runTest {
-        val savedStateHandle = buildSavedStateHandle()
+    fun `WHEN getWishlistList returns a success THEN update the state with the correct wishlist items`() =
+        runTest {
+            val savedStateHandle = buildSavedStateHandle()
 
-        coEvery { getWishlistUseCase() } returns flow {
-            emit(UseCaseResult.Success(products))
+            coEvery { getWishlistUseCase() } returns flow {
+                emit(UseCaseResult.Success(products))
+            }
+            every { wishlistUiFactory(products, any(), any(), any()) } returns wishListProductUi
+
+            val viewModel = buildViewModel(savedStateHandle)
+
+            viewModel.state.test {
+                val result = awaitItem()
+                assertEquals(WishlistUiState.Data.Loaded(wishListProductUi), result)
+                cancelAndConsumeRemainingEvents()
+            }
         }
-        every { wishlistUiFactory(products, any(), any(), any()) } returns wishListProductUi
-
-        val viewModel = buildViewModel(savedStateHandle)
-
-        viewModel.state.test {
-            val result = awaitItem()
-            assertEquals(WishlistUiState.Data.Loaded(wishListProductUi), result)
-            cancelAndConsumeRemainingEvents()
-        }
-    }
 
     @Test
-    fun `WHEN getWishlistList returns an error THEN update the state with the error state`() = runTest {
-        val savedStateHandle = buildSavedStateHandle()
+    fun `WHEN getWishlistList returns an error THEN update the state with the error state`() =
+        runTest {
+            val savedStateHandle = buildSavedStateHandle()
 
-        coEvery { getWishlistUseCase() } returns flow {
-            emit(UseCaseResult.Error(mockk()))
+            coEvery { getWishlistUseCase() } returns flow {
+                emit(UseCaseResult.Error(mockk()))
+            }
+
+            val viewModel = buildViewModel(savedStateHandle)
+
+            viewModel.state.test {
+                val result = awaitItem()
+                assertEquals(WishlistUiState.Error, result)
+                cancelAndConsumeRemainingEvents()
+            }
         }
-
-        val viewModel = buildViewModel(savedStateHandle)
-
-        viewModel.state.test {
-            val result = awaitItem()
-            assertEquals(WishlistUiState.Error, result)
-            cancelAndConsumeRemainingEvents()
-        }
-    }
 
     @Test
-    fun `WHEN onNavigateToProductDetails is called THEN navigates to ProductDetails screen with the correct productId`() = runTest {
-        val savedStateHandle = buildSavedStateHandle()
-        val productId = "123456"
-        val expected = Screen.ProductDetails(args = productDetailsNavArgs(id = productId))
+    fun `WHEN onNavigateToProductDetails is called THEN navigates to ProductDetails screen with the correct productId`() =
+        runTest {
+            val savedStateHandle = buildSavedStateHandle()
+            val productId = "123456"
+            val expected = Screen.ProductDetails(args = productDetailsNavArgs(id = productId))
 
-        val viewModel = buildViewModel(savedStateHandle)
+            val viewModel = buildViewModel(savedStateHandle)
 
-        viewModel.onNavigateToProductDetails(productId)
+            viewModel.onNavigateToProductDetails(productId)
 
-        viewModel.run {
-            verify { navigateTo(screen = expected) }
+            viewModel.run {
+                verify { navigateTo(screen = expected) }
+            }
         }
-    }
 
     @Test
-    fun `WHEN wishlist loads THEN onAddToBagClick triggers navigation to ProductDetails`() = runTest {
-        val savedStateHandle = buildSavedStateHandle()
-        val product = products.first()
-        val expected = Screen.ProductDetails(args = productDetailsNavArgs(id = product.id))
-        val onAddToBagClickSlot = slot<ClickEventOneArg<Product>>()
+    fun `WHEN wishlist loads THEN onAddToBagClick triggers navigation to ProductDetails`() =
+        runTest {
+            val savedStateHandle = buildSavedStateHandle()
+            val product = products.first()
+            val expected = Screen.ProductDetails(args = productDetailsNavArgs(id = product.id))
+            val onAddToBagClickSlot = slot<ClickEventOneArg<Product>>()
 
-        coEvery { getWishlistUseCase() } returns flow {
-            emit(UseCaseResult.Success(products))
+            coEvery { getWishlistUseCase() } returns flow {
+                emit(UseCaseResult.Success(products))
+            }
+            every {
+                wishlistUiFactory(products, any(), capture(onAddToBagClickSlot), any())
+            } returns wishListProductUi
+
+            val viewModel = buildViewModel(savedStateHandle)
+            onAddToBagClickSlot.captured(product)
+
+            viewModel.run {
+                verify { navigateTo(screen = expected) }
+            }
         }
-        every {
-            wishlistUiFactory(products, any(), capture(onAddToBagClickSlot), any())
-        } returns wishListProductUi
-
-        val viewModel = buildViewModel(savedStateHandle)
-        onAddToBagClickSlot.captured(product)
-
-        viewModel.run {
-            verify { navigateTo(screen = expected) }
-        }
-    }
 
     @Test
-    fun `WHEN wishlist loads THEN onProductClick triggers navigation to ProductDetails`() = runTest {
-        val savedStateHandle = buildSavedStateHandle()
-        val product = products.first()
-        val expected = Screen.ProductDetails(args = productDetailsNavArgs(id = product.id))
-        val onProductClickSlot = slot<ClickEventOneArg<Product>>()
+    fun `WHEN wishlist loads THEN onProductClick triggers navigation to ProductDetails`() =
+        runTest {
+            val savedStateHandle = buildSavedStateHandle()
+            val product = products.first()
+            val expected = Screen.ProductDetails(args = productDetailsNavArgs(id = product.id))
+            val onProductClickSlot = slot<ClickEventOneArg<Product>>()
 
-        coEvery { getWishlistUseCase() } returns flow {
-            emit(UseCaseResult.Success(products))
+            coEvery { getWishlistUseCase() } returns flow {
+                emit(UseCaseResult.Success(products))
+            }
+            every {
+                wishlistUiFactory(products, any(), any(), capture(onProductClickSlot))
+            } returns wishListProductUi
+
+            val viewModel = buildViewModel(savedStateHandle)
+            onProductClickSlot.captured(product)
+
+            viewModel.run {
+                verify { navigateTo(screen = expected) }
+            }
         }
-        every {
-            wishlistUiFactory(products, any(), any(), capture(onProductClickSlot))
-        } returns wishListProductUi
-
-        val viewModel = buildViewModel(savedStateHandle)
-        onProductClickSlot.captured(product)
-
-        viewModel.run {
-            verify { navigateTo(screen = expected) }
-        }
-    }
 
     private fun buildSavedStateHandle() = mockk<SavedStateHandle>(relaxed = true).also {
         every { it.get<Boolean>("launchFromTop") } returns false
